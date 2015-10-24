ab39f21f0658712bc43953c7facd291520233bcf
Sergey Malgin <malginsa@gmail.com>
2015-10-24 10:42:26 +0300

    HomeWork151019\create_table.sql added

---
+++ HomeWork151019/create_table.sql
@@ -0,0 +1,8 @@
+create database family;
+
+use family;
+
+create table person (id int primary key auto_increment, name varchar(255), dob date, height int, weight real);
+
+# insert into person values (0, "Askur", "1016-4-1", 170, 70.1);
+
